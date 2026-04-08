f4de5ee00e2c7cc2e1146ed9341b71423db7e154
lruda1 <lruda1@stu.vistula.edu.pl>
2026-04-08 17:18:41 +0300

    77285 zad5: dodanie formularza kontaktowego i walidacji

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-// елементи
+
 const themeBtn = document.getElementById("themeBtn");
 const toggleBtn = document.getElementById("toggleBtn");
 const projects = document.getElementById("projects");
 const themeStyle = document.getElementById("theme-style");
 
-// змінна для теми
 let isGreen = true;
 
-// зміна теми
 themeBtn.addEventListener("click", () => {
     if (isGreen) {
         themeStyle.href = "red.css";
@@ -18,11 +16,62 @@ themeBtn.addEventListener("click", () => {
     }
 });
 
-// показати / сховати секцію
 toggleBtn.addEventListener("click", () => {
-    if (projects.style.display === "none") {
+    if (projects.style.display === "none" || projects.style.display === "") {
         projects.style.display = "block";
+        toggleBtn.textContent = "Ukryj projekty";
     } else {
         projects.style.display = "none";
+        toggleBtn.textContent = "Pokaż projekty";
+    }
+});
+
+
+const form = document.getElementById("contactForm");
+
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+
+const firstNameError = document.getElementById("firstNameError");
+const lastNameError = document.getElementById("lastNameError");
+const emailError = document.getElementById("emailError");
+const messageError = document.getElementById("messageError");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    let isValid = true;
+
+    firstNameError.textContent = "";
+    lastNameError.textContent = "";
+    emailError.textContent = "";
+    messageError.textContent = "";
+
+    if (firstName.value.trim() === "" || /\d/.test(firstName.value)) {
+        firstNameError.textContent = "Niepoprawne imię";
+        isValid = false;
+    }
+
+    if (lastName.value.trim() === "" || /\d/.test(lastName.value)) {
+        lastNameError.textContent = "Niepoprawne nazwisko";
+        isValid = false;
+    }
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email.value)) {
+        emailError.textContent = "Niepoprawny email";
+        isValid = false;
+    }
+
+    if (message.value.trim() === "") {
+        messageError.textContent = "Wiadomość jest wymagana";
+        isValid = false;
+    }
+
+    if (isValid) {
+        alert("Formularz wysłany poprawnie!");
+        form.reset();
     }
 });
